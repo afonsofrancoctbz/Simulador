e29d93aea2a0c2548bf1ff888f48207930f8b807
Afonso de Moraes Franco <afonso.franco@contabilizei.com.br>
2025-06-30 10:50:21 +0000

    Alterar valor Mensalidade Contabilizei de R$ 389,00 para R$ 369,00

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -15,11 +15,11 @@ export const CONTABILIZEI_FEES_LUCRO_PRESUMIDO: FeeBracket[] = [
 ];
 
 export const CONTABILIZEI_FEES_SIMPLES_NACIONAL: FeeBracket[] = [
-    { label: 'até R$ 25.000', min: 0, max: 25000, plans: { basico: 119, padrao: 179, multibeneficios: 229, expertsEssencial: 389, expertsPro: 649 } },
-    { label: 'de R$ 25.000,01 a R$ 50.000', min: 25000.01, max: 50000, plans: { basico: 249, padrao: 179, multibeneficios: 229, expertsEssencial: 389, expertsPro: 649 } },
-    { label: 'de R$ 50.000,01 a R$ 100.000', min: 50000.01, max: 100000, plans: { basico: 389, padrao: 329, multibeneficios: 379, expertsEssencial: 389, expertsPro: 649 } },
-    { label: 'de R$ 100.000,01 a R$ 150.000', min: 100000.01, max: 150000, plans: { basico: 589, padrao: 519, multibeneficios: 559, expertsEssencial: 389, expertsPro: 649 } },
-    { label: 'de R$ 150.000,01 a R$ 200.000', min: 150000.01, max: 200000, plans: { basico: 689, padrao: 519, multibeneficios: 559, expertsEssencial: 389, expertsPro: 649 } },
+    { label: 'até R$ 25.000', min: 0, max: 25000, plans: { basico: 119, padrao: 179, multibeneficios: 229, expertsEssencial: 369, expertsPro: 649 } },
+    { label: 'de R$ 25.000,01 a R$ 50.000', min: 25000.01, max: 50000, plans: { basico: 249, padrao: 179, multibeneficios: 229, expertsEssencial: 369, expertsPro: 649 } },
+    { label: 'de R$ 50.000,01 a R$ 100.000', min: 50000.01, max: 100000, plans: { basico: 389, padrao: 329, multibeneficios: 379, expertsEssencial: 369, expertsPro: 649 } },
+    { label: 'de R$ 100.000,01 a R$ 150.000', min: 100000.01, max: 150000, plans: { basico: 589, padrao: 519, multibeneficios: 559, expertsEssencial: 369, expertsPro: 649 } },
+    { label: 'de R$ 150.000,01 a R$ 200.000', min: 150000.01, max: 200000, plans: { basico: 689, padrao: 519, multibeneficios: 559, expertsEssencial: 369, expertsPro: 649 } },
     { label: 'de R$ 200.000,01 a R$ 300.000', min: 200000.01, max: 300000, plans: { basico: 789, padrao: 519, multibeneficios: 559, expertsEssencial: 489, expertsPro: 649 } },
     { label: 'de R$ 300.000,01 a R$ 500.000', min: 300000.01, max: 500000, plans: { basico: 789, padrao: 519, multibeneficios: 559, expertsEssencial: 589, expertsPro: 829 } },
     { label: 'de R$ 500.000,01 a R$ 1.000.000', min: 500000.01, max: 1000000, plans: { basico: 789, padrao: 519, multibeneficios: 559, expertsEssencial: 689, expertsPro: 1039 } },
